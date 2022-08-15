4d94740f2eaa8a4f7497994d0e1aa33e3cca9143
Jonika <katyadyduch@gmail.com>
2022-08-15 21:44:24 +0300

    Добавьте файлы проекта.

---
+++ FirstDiscordBot/Commands.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+
+namespace FirstDiscordBot
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        public async Task Pincmd()
+        {
+            await Context.Message.ReplyAsync("ping"); //ответить тегая
+        }
+        [Command("del")]
+        [Summary("Deletes a specified amount of messages")]
+        [RequireBotPermission(GuildPermission.ManageMessages)]
+        [RequireUserPermission(GuildPermission.ManageMessages)]
+        public async Task DelMesAsync(int delnum)
+        {
+            var messages = Context.Channel.GetMessagesAsync(delnum).Flatten();
+            foreach (var h in await messages.ToArrayAsync())
+            {
+                await this.Context.Channel.DeleteMessageAsync(h);
+            }
+        }
+        [Command("createLog")]
+        [RequireUserPermission(GuildPermission.Administrator)]
+        private async Task Log()
+        {
+            var channel = Context.Guild.Channels.SingleOrDefault(x => x.Name == "log");
+
+            if (channel == null)  
+            {
+                
+                var newChannel = await Context.Guild.CreateTextChannelAsync("log");
+                var id = Context.Message.Author.Id;
+                var newChannelId = newChannel.Id;
+                await ReplyAsync("Created channel ``'log'``");
+                //await newChannel.AddPermissionOverwriteAsync(AddGuildUserProper.EveryoneRole, OverwritePermissions.DenyAll(newChannel));
+
+            }
+            else // reply that the channel already exists
+            {
+                await ReplyAsync("Unable to create channel ``log`` channel already exists.");
+            }
+        }
+        
+        [Command("random")]
+        public async Task RandomCommand(int min , int max)
+        {
+            Random r = new Random();
+            await Context.Message.ReplyAsync(r.Next(min, max).ToString());
+        }
+        [Command("avatar")]
+        public async Task AvatarCommand(IGuildUser u)
+        {
+            var users = Context.Message.MentionedUsers;
+            var subject = users.First();
+            var embedBuiler = new EmbedBuilder()
+            .WithImageUrl(subject.GetAvatarUrl( ImageFormat.Auto , size:300))
+            .WithColor(Color.Green)
+            .WithCurrentTimestamp();
+            await Context.Message.ReplyAsync(embed: embedBuiler.Build());
+        }
+        [Command("kiss")]
+        public async Task KissCommand(IGuildUser u)
+        {
+            Random random = new Random();
+            var b = new EmbedBuilder().
+              WithDescription($"**{Context.User.Mention} поцеловал(а) {u.Mention}!**\n");
+           
+            int num = random.Next(1, 4);
+            switch (num)
+            {
+                case 1:
+                    b.WithImageUrl("https://i.gifer.com/Dcnz.gif");
+                break;
+                    case 2:
+                    b.WithImageUrl("https://c.tenor.com/I8kWjuAtX-QAAAAC/anime-ano.gif");
+                    break;
+                case 3:
+                    b.WithImageUrl("https://c.tenor.com/NO6j5K8YuRAAAAAC/leni.gif");
+                    break;
+                case 4:
+                    b.WithImageUrl("https://c.tenor.com/9jB6M6aoW0AAAAAS/val-ally-kiss.gif");
+                    break;
+
+            }
+            Embed embed = b.Build();
+            await ReplyAsync(embed: embed);
+         }
+        [Command("hit")]
+        public async Task HitCommand(IGuildUser u)
+        {
+            Random random = new Random();
+            var b = new EmbedBuilder().
+              WithDescription($"**{Context.User.Mention} ударил(а) {u.Mention}!**\n");
+
+            int num = random.Next(1, 4);
+            switch (num)
+            {
+                case 1:
+                    b.WithImageUrl("https://c.tenor.com/1T5bgBYtMgUAAAAS/head-hit-anime.gif");
+                    break;
+                case 2:
+                    b.WithImageUrl("https://c.tenor.com/Y8_ITfFMQmMAAAAC/yue-arifureta.gif");
+                    break;
+                case 3:
+                    b.WithImageUrl("https://c.tenor.com/Uw63A_uIWE8AAAAS/ijiranaide-nagatoro-anime.gif");
+                    break;
+                case 4:
+                    b.WithImageUrl("https://c.tenor.com/g_9NDHUmUdgAAAAC/anime.gif");
+                    break;
+
+            }
+            Embed embed = b.Build();
+            await ReplyAsync(embed: embed);
+         }
+        [Command("hug")]
+        public async Task HugCommand(IGuildUser u)
+        {
+            Random random = new Random();
+            var b = new EmbedBuilder().
+              WithDescription($"**{Context.User.Mention} обнял(а) {u.Mention}!**\n");
+
+            int num = random.Next(1, 4);
+            switch (num)
+            {
+                case 1:
+                    b.WithImageUrl("https://c.tenor.com/XyMvYx1xcJAAAAAS/super-excited.gif");
+                    break;
+                case 2:
+                    b.WithImageUrl("https://c.tenor.com/J7eGDvGeP9IAAAAS/enage-kiss-anime-hug.gif");
+                    break;
+                case 3:
+                    b.WithImageUrl("https://c.tenor.com/lzKyZchfMzAAAAAS/anime-hug.gif");
+                    break;
+                case 4:
+                    b.WithImageUrl("https://c.tenor.com/V3GQKvQjej0AAAAS/love-gif-sad.gif");
+                    break;
+
+            }
+            Embed embed = b.Build();
+            await ReplyAsync(embed: embed);
+        }
+        [Command("SonyaLox")]
+        public async Task GetMyIdAsync()
+        {
+            var builder = new ComponentBuilder()
+            .WithButton("Да", "1",ButtonStyle.Success)
+            .WithButton("Нет", "2", ButtonStyle.Danger);
+          
+            await Context.Message.ReplyAsync("Соня лох?", components: builder.Build() ); 
+        }
+         
+        [Command("Ban")]
+        [RequireBotPermission(GuildPermission.BanMembers, ErrorMessage = "U dont can")]
+        public async Task BanCommand(IGuildUser user , string reason  )
+        {
+       
+            if (user == null)
+            {
+                await ReplyAsync("User == null");
+                return;
+            }
+            var embedBuilder = new EmbedBuilder()
+            .WithDescription($":white_check_mark: {user.Mention} В бан пошла\n**причина:** {reason}")
+            .WithColor(new Color(255, 0, 0));
+            Embed embed = embedBuilder.Build();
+            await ReplyAsync(embed: embed);
+            await Context.Guild.AddBanAsync(user,1,reason);
+
+        }
+        [Command("Kick")]
+        [RequireBotPermission(GuildPermission.KickMembers, ErrorMessage = "U dont can")]
+        public async Task KickCommand (IGuildUser user , string reason)
+        {
+            if(user == null)
+            {
+                await ReplyAsync("user == null");
+                    return;
+            }
+            var embedBuilder = new EmbedBuilder()
+             .WithDescription($":white_check_mark: {user.Mention} В кик пошла\n**причина:** {reason}")
+             .WithColor(Color.DarkBlue);
+            Embed embed = embedBuilder.Build();
+            await ReplyAsync(embed: embed);
+            // Channel
+            //var channel = _client.channels.cache.find(channel => channel.name == "Log")
+            //channel.send(message)
+            await user.KickAsync();
+        }
+
+        [Command("mommy")]
+        public async Task RoleMommy()
+        {
+            var user = Context.User;
+            var role = Context.Guild.Roles.FirstOrDefault(x => x.Name == "Мамка сервера");
+            await (user as IGuildUser).AddRoleAsync(role);
+            var embedBuilder = new EmbedBuilder()
+           .WithDescription($":white_check_mark: {user.Mention} Ты получил роль {role.Name}")
+           .WithColor(new Color(255, 0, 0));
+
+        }
+    }
+}
---
+++ FirstDiscordBot/Program.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Discord.Interactions;
+using Microsoft.Extensions.Hosting;
+using FirstDiscordBot;
+using Discord.Net;
+
+class Program
+{
+    private DiscordSocketClient _client;
+    private CommandService commands;
+    private IServiceProvider services;
+
+
+    static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+
+    public async Task MainAsync()
+    {
+        commands = new CommandService();
+        var _config = new DiscordSocketConfig
+        {
+            AlwaysDownloadUsers = true,
+            MessageCacheSize = 1000,
+            UseInteractionSnowflakeDate = false,
+            GatewayIntents =
+                GatewayIntents.Guilds |
+                GatewayIntents.GuildMembers |
+                GatewayIntents.GuildMessageReactions |
+                GatewayIntents.GuildMessages |
+                GatewayIntents.GuildVoiceStates
+        };
+
+        _client = new DiscordSocketClient(_config);
+        string token = "token";
+        _client.Log += LogAsync;
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+        _client.Ready += _client_Ready;
+
+        await commands.AddModulesAsync(Assembly.GetEntryAssembly(), services);
+       
+ 
+        _client.MessageUpdated += MessageUpdated;
+        _client.MessageDeleted += _client_MessageDeleted;
+        _client.MessageReceived += HandleCommandAsync;
+        _client.SlashCommandExecuted += _client_SlashCommandExecuted;
+        _client.ButtonExecuted += _client_ButtonExecuted;
+
+        await Task.Delay(-1);
+    }
+    private async Task LogAsync(LogMessage message)
+           => Console.WriteLine(message.ToString());
+  
+
+    private async Task _client_ButtonExecuted(SocketMessageComponent arg)
+    {
+        switch (arg.Data.CustomId)
+        {
+            case "1":
+                await arg.RespondAsync($"{arg.User.Mention} its true!");
+                break;
+        }
+    }
+
+    private async Task _client_Ready()
+    { 
+        var guildCommand1 = new SlashCommandBuilder();
+        guildCommand1.WithName("help");
+        guildCommand1.WithDescription("Command List");
+        var guildCommand = new SlashCommandBuilder()
+        .WithName("list-roles")
+        .WithDescription("Lists all roles of a user.")
+        .AddOption("user", ApplicationCommandOptionType.User, "The users whos roles you want to be listed", isRequired: true);
+ 
+        try
+        {
+            await _client.CreateGlobalApplicationCommandAsync(guildCommand.Build());
+            await _client.CreateGlobalApplicationCommandAsync(guildCommand1.Build());
+        }
+        catch (ApplicationCommandException exception)
+        {
+
+        }
+     
+    }
+
+    private async Task _client_SlashCommandExecuted(SocketSlashCommand command)
+    {
+        switch (command.Data.Name)
+        {
+            case "list-roles":
+                await HandleListRoleCommand(command);
+                break;
+            case "help":
+                await HandleHelpCommand(command);
+                break;
+
+        }
+    }
+
+
+    private async Task HandleHelpCommand(SocketSlashCommand command)
+    {
+
+        var embedBuiler = new EmbedBuilder()
+           .WithTitle("All commands:")
+           .AddField("For users", "**!kiss @tag - u kiss someone**\n" +
+           "**!hug @tag - u hug someone**\n" + "**!hit @tag - u hit someone**\n" + "**!avatar @tag - view user avatar**\n"
+           + "**!random min,max - random num in the range\n**")
+           .AddField("All commands for admin:\n", "**!kick @tag, reason- u kick someone**\n" +
+           "**!ban @tag, time, reason - u ban someone**\n" + "**!del count - deletes a certain number of messages**\n"
+           + "**!createLog - create a channel in which there will be deletion, message editing logs**")
+           .WithColor(Color.Green)
+           .WithCurrentTimestamp();
+
+        await command.RespondAsync(embed: embedBuiler.Build());
+    }
+   
+    private async Task HandleListRoleCommand(SocketSlashCommand command)
+    {
+        var guildUser = (SocketGuildUser)command.Data.Options.First().Value;
+        var roleList = string.Join(",\n", guildUser.Roles.Where(x => !x.IsEveryone).Select(x => x.Mention));
+        var embedBuiler = new EmbedBuilder()
+            .WithAuthor(guildUser.ToString(), guildUser.GetAvatarUrl() ?? guildUser.GetDefaultAvatarUrl())
+            .WithTitle("Roles")
+            .WithDescription(roleList)
+            .WithColor(Color.Green)
+            .WithCurrentTimestamp();
+
+        await command.RespondAsync(embed: embedBuiler.Build());
+    }
+
+    private async Task _client_MessageDeleted(Cacheable<IMessage, ulong> arg1, Cacheable<IMessageChannel, ulong> arg2)
+    {
+        IMessage msg = await arg1.GetOrDownloadAsync();
+        var channel = msg.Channel;
+        ulong GuidC = GetGuild((ISocketMessageChannel)channel);
+        if (GuidC != 0)
+        {
+            var chnl = _client.GetChannel(GuidC) as IMessageChannel;
+            if (msg != null)
+            {
+                Console.WriteLine($"Delete message: \"{msg.Content}\" , from \"{msg.Channel}\"");
+                await chnl.SendMessageAsync($"Delete message: \"{msg.Content}\" , from \"{msg.Channel}\" , author:{msg.Author}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("No log channel");
+        }
+    }
+    private ulong GetGuild(ISocketMessageChannel channel)
+    {
+        ulong tmp = 0;
+        SocketGuild server = ((SocketGuildChannel)channel).Guild;//get server guid
+        var guild = _client.GetGuild(server.Id);
+        var channels = guild.Channels.Where(x => !(x is ICategoryChannel));//get list of text channels
+        foreach (var c in channels)
+        {
+            if (c.Name == "log")
+            {
+                tmp = c.Id;
+                break;
+            }
+        }
+        return tmp;
+    }
+    private async Task MessageUpdated(Cacheable<IMessage, ulong> before, SocketMessage after, ISocketMessageChannel channel)
+    {
+        ulong GuidC = GetGuild(channel);
+
+        if (GuidC != 0)
+        {
+            var chnl = _client.GetChannel(GuidC) as IMessageChannel;
+            var msg = await before.GetOrDownloadAsync();
+            if (msg != null)
+            {
+                Console.WriteLine($"{msg} -> {after}");
+                await chnl.SendMessageAsync($"Edit message: \"{msg}\"-> \"{after}\" , from \"{msg.Channel}\" author:\"{msg.Author}\"");
+            }
+        }
+        else
+        {
+            Console.WriteLine("No log channel");
+        }
+
+    }
+    private async Task HandleCommandAsync(SocketMessage arg)
+    {
+        try
+        {
+            var message = arg as SocketUserMessage;
+            var context = new SocketCommandContext(_client, message);
+            if (message.Author.IsBot)
+                return;
+
+            int argPos = 0;
+            if (message.HasStringPrefix("!", ref argPos))
+            {
+                var result = await commands.ExecuteAsync(context, argPos, services);
+                if (!result.IsSuccess)
+                    Console.WriteLine(result.ErrorReason);
+                if (result.Error.Equals(CommandError.UnmetPrecondition))
+                    await message.Channel.SendMessageAsync(result.ErrorReason);
+            }
+        }
+        catch
+        {
+
+        }
+    }
+}
